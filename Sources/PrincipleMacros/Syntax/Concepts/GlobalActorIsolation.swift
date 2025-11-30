eaccbf0da2f83def22400deee58da2d937744e92
Kamil Strzelecki <strzeleckikamil.developer@gmail.com>
2025-11-30 19:15:03 +0100

    -

--- Sources/PrincipleMacros/Syntax/Concepts/GlobalActorIsolation.swift
+++ Sources/PrincipleMacros/Syntax/Concepts/GlobalActorIsolation.swift
@@ -13,6 +13,14 @@ public enum GlobalActorIsolation {
     case nonisolated(trimmedModifer: DeclModifierSyntax)
     case isolated(standardizedType: TypeSyntax)
 
+    public static var nonisolated: Self {
+        let modifier = DeclModifierSyntax(name: .keyword(.nonisolated))
+        return .nonisolated(trimmedModifer: modifier)
+    }
+}
+
+extension GlobalActorIsolation {
+
     public var trimmedNonisolatedModifier: DeclModifierSyntax? {
         switch self {
         case let .nonisolated(trimmedModifer):
